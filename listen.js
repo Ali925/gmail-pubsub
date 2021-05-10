7ca0b760acc429bff943d5a5106043bb9c3b71c1
Ali925 <alitural.mehdi@gmail.com>
2021-05-10 13:23:11 +0400

    first commit

---
+++ listen.js
@@ -0,0 +1,36 @@
+
+const subscriptionName = 'projects/sturdy-magnet-236118/subscriptions/receive-email';
+const timeout = 60000;
+
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
+
+// Creates a client; cache this for further use
+const pubSubClient = new PubSub({projectId: "sturdy-magnet-236118"});
+
+function listenForMessages() {
+  // References an existing subscription
+  const subscription = pubSubClient.subscription(subscriptionName);
+
+  // Create an event handler to handle messages
+  let messageCount = 0;
+  const messageHandler = message => {
+    console.log(`Received message ${message.id}:`);
+    console.log(message.data.toString());                     
+    console.log('message attr: ', message.attributes);
+    messageCount += 1;
+
+    // "Ack" (acknowledge receipt of) the message
+    message.ack();
+  };
+
+  // Listen for new messages until timeout is hit
+  subscription.on('message', messageHandler);
+
+  setTimeout(() => {
+    subscription.removeListener('message', messageHandler);
+    console.log(`${messageCount} message(s) received.`);
+  }, timeout * 1000);
+}
+
+listenForMessages();
